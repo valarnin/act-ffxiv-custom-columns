6acbc88c306cdd7a331e21125267ad8a89fc7770
valarnin <valarnin@gmail.com>
2021-05-17 19:38:58 -0400

    - Rewrite to prep for some day making a config GUI - Add missing Angel Whisper rez

--- CustomColumns.cs
+++ CustomColumns.cs
@@ -1,6 +1,7 @@
 using System.Windows.Forms;
 using Advanced_Combat_Tracker;
 using System.Reflection;
+using System.Collections.Generic;
 
 [assembly: AssemblyTitle("Custom columns")]
 [assembly: AssemblyDescription("Adds custom columns for FFXIV")]
@@ -9,9 +10,84 @@ using System.Reflection;
 
 namespace ACT_Plugin
 {
-  public class PluginSample : IActPluginV1
+  public class CustomColumnDef
+  {
+    public List<string> IncomingAbilities = new List<string>();
+    public List<string> OutgoingAbilities = new List<string>();
+    public List<string> EitherAbilities = new List<string>();
+    public string ExportName;
+    public string Name;
+    public string Description;
+
+    private List<string> calculatedIncomingAbilities = new List<string>();
+    private List<string> calculatedOutgoingAbilities = new List<string>();
+
+    public void Initialize()
+    {
+      foreach (var ability in this.IncomingAbilities)
+        if (!this.calculatedIncomingAbilities.Contains(ability))
+          this.calculatedIncomingAbilities.Add(ability);
+
+      foreach (var ability in this.OutgoingAbilities)
+        if (!this.calculatedOutgoingAbilities.Contains(ability))
+          this.calculatedOutgoingAbilities.Add(ability);
+
+      foreach (var ability in this.EitherAbilities) {
+        if (!this.calculatedIncomingAbilities.Contains(ability))
+          this.calculatedIncomingAbilities.Add(ability);
+        if (!this.calculatedOutgoingAbilities.Contains(ability))
+          this.calculatedOutgoingAbilities.Add(ability);
+      }
+
+      CombatantData.ColumnDefs.Add(Name,
+          new CombatantData.ColumnDef(Name, true, "INT", Name,
+            (Data) => {
+              return GetCount(Data).ToString();
+            },
+            (Data) => {
+              return GetCount(Data).ToString();
+            },
+            (Left, Right) => {
+              return GetCount(Left).CompareTo(GetCount(Right));
+            }
+          )
+        );
+
+        // Make sure the new column is in the Options tab
+        ActGlobals.oFormActMain.ValidateTableSetup();
+
+        CombatantData.ExportVariables.Add(ExportName,
+          new CombatantData.TextExportFormatter(ExportName, Name, Description,
+            (Data, Extra) => {
+              return GetCount(Data).ToString();
+            }
+          )
+        );
+    }
+
+    public int GetCount(CombatantData Data) {
+      int count = 0;
+      AttackType atk = null;
+
+      foreach (var ability in this.calculatedIncomingAbilities)
+      {
+        if (Data.AllInc.TryGetValue(ability, out atk))
+          count += atk.Swings;
+      }
+      foreach (var ability in this.calculatedOutgoingAbilities)
+      {
+        if (Data.AllOut.TryGetValue(ability, out atk))
+          count += atk.Swings;
+      }
+      return count;
+    }
+  }
+
+  public class CustomColumns : IActPluginV1
   {
     Label lblStatus;  // The status label that appears in ACT's Plugin tab
+
+    List<CustomColumnDef> Columns;
 
     public void DeInitPlugin()
     {
@@ -21,134 +97,48 @@ namespace ACT_Plugin
     public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
     {
       lblStatus = pluginStatusText;  // Hand the status label's reference to our local var
-      InitRezesColumn();
-      InitDamageDownColumn();
-      InitVulnerabilityUpColumn();
+      this.Columns = new List<CustomColumnDef>();
+      Columns.Add(new CustomColumnDef()
+      {
+        ExportName = "rezes",
+        Name = "Rezes",
+        Description = "Number of ressurections cast by the combatant.",
+        OutgoingAbilities = new List<string>(){
+          "Resurrection",
+          "Raise",
+          "Ascend",
+          "Angel Whisper"
+        },
+      });
+      Columns.Add(new CustomColumnDef()
+      {
+        ExportName = "dmgdown",
+        Name = "Dmg Down",
+        Description = "Number of Damage Downs received by the combatant.",
+        IncomingAbilities = new List<string>(){
+          "Damage Down"
+        },
+      });
+      Columns.Add(new CustomColumnDef()
+      {
+        ExportName = "vulns",
+        Name = "Vulns",
+        Description = "Number of Vulnerability Ups received by the combatant.",
+        IncomingAbilities = new List<string>(){
+          "Vulnerability Up"
+        },
+      });
+      InitColumns();
       ActGlobals.oFormActMain.ValidateLists();  // Make sure the new variable appears in the preset creator
       lblStatus.Text = "Plugin Started";
     }
 
-    private void InitRezesColumn() {
-      CombatantData.ColumnDefs.Add("Rezes",
-        new CombatantData.ColumnDef("Rezes", true, "INT", "Rezes",
-          (Data) => {
-            return GetRessurectionCount(Data).ToString();
-          },
-          (Data) => {
-            return GetRessurectionCount(Data).ToString();
-          },
-          (Left, Right) => {
-            return GetRessurectionCount(Left).CompareTo(GetRessurectionCount(Right));
-          }
-        )
-      );
-
-      // Make sure the new column is in the Options tab
-      ActGlobals.oFormActMain.ValidateTableSetup();
-
-      CombatantData.ExportVariables.Add("rezes",
-        new CombatantData.TextExportFormatter("rezes", "Ressurections", "Number of ressurections cast by the combatant.",
-          (Data, Extra) => {
-            return GetRessurectionCount(Data).ToString();
-          }
-        )
-      );
-    }
-
-    private int GetRessurectionCount(CombatantData Combatant)
+    private void InitColumns()
     {
-      int count = 0;
-
-      AttackType atk = null;
-
-      if (Combatant.AllOut.TryGetValue("Resurrection", out atk))
-        count += atk.Swings;
-
-      if (Combatant.AllOut.TryGetValue("Raise", out atk))
-        count += atk.Swings;
-
-      if (Combatant.AllOut.TryGetValue("Ascend", out atk))
-        count += atk.Swings;
-
-      return count;
-    }
-
-    private void InitDamageDownColumn() {
-      CombatantData.ColumnDefs.Add("Dmg Down",
-        new CombatantData.ColumnDef("Dmg Down", true, "INT", "Dmg Down",
-          (Data) => {
-            return GetDamageDownCount(Data).ToString();
-          },
-          (Data) => {
-            return GetDamageDownCount(Data).ToString();
-          },
-          (Left, Right) => {
-            return GetDamageDownCount(Left).CompareTo(GetDamageDownCount(Right));
-          }
-        )
-      );
-
-      // Make sure the new column is in the Options tab
-      ActGlobals.oFormActMain.ValidateTableSetup();
-
-      CombatantData.ExportVariables.Add("dmgdown",
-        new CombatantData.TextExportFormatter("dmgdown", "Damage Down", "Number of Damage Downs received by the combatant.",
-          (Data, Extra) => {
-            return GetDamageDownCount(Data).ToString();
-          }
-        )
-      );
-    }
-
-    private int GetDamageDownCount(CombatantData Combatant)
-    {
-      int count = 0;
-
-      AttackType atk = null;
-
-      if (Combatant.AllInc.TryGetValue("Damage Down", out atk))
-        count += atk.Swings;
-
-      return count;
-    }
-
-    private void InitVulnerabilityUpColumn() {
-      CombatantData.ColumnDefs.Add("Vulns",
-        new CombatantData.ColumnDef("Vulns", true, "INT", "Vulns",
-          (Data) => {
-            return GetVulnerabilityUpCount(Data).ToString();
-          },
-          (Data) => {
-            return GetVulnerabilityUpCount(Data).ToString();
-          },
-          (Left, Right) => {
-            return GetVulnerabilityUpCount(Left).CompareTo(GetVulnerabilityUpCount(Right));
-          }
-        )
-      );
-
-      // Make sure the new column is in the Options tab
-      ActGlobals.oFormActMain.ValidateTableSetup();
-
-      CombatantData.ExportVariables.Add("vulns",
-        new CombatantData.TextExportFormatter("vulns", "Vulnerability Up", "Number of Vulnerability Ups received by the combatant.",
-          (Data, Extra) => {
-            return GetVulnerabilityUpCount(Data).ToString();
-          }
-        )
-      );
-    }
-
-    private int GetVulnerabilityUpCount(CombatantData Combatant)
-    {
-      int count = 0;
-
-      AttackType atk = null;
-
-      if (Combatant.AllInc.TryGetValue("Vulnerability Up", out atk))
-        count += atk.Swings;
-
-      return count;
+      foreach (var column in this.Columns)
+      {
+        column.Initialize();
+      }
     }
   }
 }
